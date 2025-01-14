9974bdb17c9c3185a9ce0434b78f48c4b6b32335
CHIRAG <74717424+ichiragkumar@users.noreply.github.com>
2025-01-14 11:38:02 +0530

    Update inhertitanceInExample.sol

--- inhertitanceInExample.sol
+++ inhertitanceInExample.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+
+contract Vehicle {
+    string brandName;
+
+    constructor(string memory _brand){
+        brandName = _brand;
+    }
+
+    function description() public pure virtual  returns(string memory){
+        return "I am a vehicle";
+    }
+}
+
+
+From 
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Vehicle {
+    string brandName;
+
+    constructor(string memory _brand){
+        brandName = _brand;
+    }
+
+    function description() public pure virtual  returns(string memory){
+        return "I am a vehicle";
+    }
+}
