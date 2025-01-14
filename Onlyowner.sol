85a8bfc25f0a11be7d85b7e3c5cd71f3b622d2fc
CHIRAG <74717424+ichiragkumar@users.noreply.github.com>
2025-01-14 18:43:09 +0530

    Update Onlyowner.sol

--- Onlyowner.sol
+++ Onlyowner.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+
+contract OnlyOwner {
+
+    uint public totalAmount;
+    address userAddress;
+
+    constructor() {
+        userAddress = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(userAddress == msg.sender, "You are not the Real Owner ");
+        _;
+    }
+
+
+    function addAmount(uint recievedAmount) public  onlyOwner {
+        totalAmount += recievedAmount;
+
+    }
+
+    function subAmmount(uint recievedAmount) public onlyOwner {
+        require(recievedAmount <= 0 , "can not be less than 0");
+        totalAmount -= recievedAmount;
+    }
+
+
+    function getToatalBalcnes() public  view returns (uint) {
+        return totalAmount;
+    }
+}
